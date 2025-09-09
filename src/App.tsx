c11c1bfe40a0b1648284f21551e698fe424dec16
BBee98 <blue98.bee.dev@gmail.com>
2025-09-09 21:57:55 +0200

    Init project! Start with css variables and remove default code

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { RMBForm } from './components/form'
+
+function App() {
+
+  return (
+    <section>
+        <nav>
+
+        </nav>
+        <RMBForm />
+    </section>
+  )
+}
+
+export default App
